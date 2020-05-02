3e42ac939ae1032447bff5d7e7b582eece8a9be2
Daelton Dias <daelton.dias@gmail.com>
2020-05-02 05:27:19 -0300

    Update

--- www/KeyboardConstroller.js
+++ www/KeyboardConstroller.js
@@ -1,28 +1,6 @@
-//var argscheck = require('cordova/argscheck');
-//var channel = require('cordova/channel');
 var exec = require('cordova/exec');
-//var cordova = require('cordova');
 
-//channel.createSticky('onCordovaInfoReady');
-//channel.waitForInitialization('onCordovaInfoReady');
-
-function KeyboardConstroller() {
-	this.version = '1.0.0';
-	//channel.onCordovaReady.subscribe(function() {
-	//	self.getInfo(
-	//		function() {
-	//			//channel.onCordovaInfoReady.fire();
-	//		},
-	//		function(e) {
-	//			console.error('[ERROR] Error initializing cordova-plugin-keyboard-controller: ' + e);
-	//		}
-	//	);
-	//});
-}
-
-KeyboardConstroller.prototype.resizeWebview = function(arg0, success, error) {
-	//argscheck.checkArgs('fF', 'Device.getInfo', arguments);
+exports.resizeWebview = function(arg0, success, error) {
   exec(success, error, 'KeyboardConstroller', 'resizeWebview', [arg0]);
 };
 
-module.exports = new KeyboardConstroller();
